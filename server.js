cc92a49d27a1f89791a2d2dca5904fb3a92db34c
Tara Maddox <TMaddox85@gmail.com>
2023-02-09 19:31:57 -0800

    1st attempt

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+console.log(PORT)
+
+// DEPENDENCIES
+const express = require('express')
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
